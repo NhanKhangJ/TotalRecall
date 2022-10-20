d7eb2780aa39637daa2275e59c2d815ea97d7c9e
NhanKhangJ <khangle51096@gmail.com>
2022-10-20 11:46:47 -0600

    fisrt commit

--- app.js
+++ app.js
@@ -1,10 +1,69 @@
+                     /*Variable & Data types */
+// 1. How do we assign a value to a variable?
+// let/var/const Varibale = "Hi" 
+
+// 2. How do we change the value of a variable?
+// Variable = "Hello" 
+
+// 3. How do we assign an existing variable to a new variable?
+// let variable1 = 10;
+// let variable2 = variable1;
+
+// 4. Remind me, what are declare, assign, and define?
+// Declare is put a value or diffrent data type inside variable
+// Assign is replace the existing value with a diffrent value 
+// define some variable with name and data types
+
+//5. What is pseudocoding and why should you do it?
+ //  simplified programming language, used in program design.
+
+//6. What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+ // 1hour
+
+ 
 // STRINGS 
 
+let firstVariable = "Hello World";
+firstVariable = 25;
+let secondVariable = firstVariable;
+secondVariable = "change to a string";
+
+var yourName = "Nhan Khang Le";
+     
+console.log(`Hello, my name is ${yourName}`) // Hello, my name is Nhan Khang Le
 
 
 //BOOLEANS
+const a = 4; 
+const b = 53; 
+const c = 57; 
+const d = 16; 
+const e = 'Kevin';
+
+console.log(a < b);
+console.log(c > d); 
+console.log('Name' == 'Name');
+//use && OR 
+
+console.log(true || false); 
+console.log(false && false && false && false && false || true); 
+console.log(!false || false)
+ console.log(e == 'Kevin'); 
+console.log(a + b == c);
+console.log(a * a == d);
+console.log(48 == '48');
+
+ // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations) console.log(a __ a ___ d); // note: the answer is a simple arithmetic equation, not something "weird" console.log(48 __ '48');
+
 
 //the Farm
+let animal = "cow";
+// if(animal === "cow"){
+//     console.log('Moo')
+// } else {
+//     console.log('This animal is not a cow')
+// }
+animal === "cow" ? console.log('Moo') : console.log('Hey! You are not a cow')
 
 //Driver's Ed
 
